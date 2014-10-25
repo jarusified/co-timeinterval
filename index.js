b7edb789947549d01717be22591f6d892801f8fd
jarusified <jarus3001@gmail.com>
2014-10-25 20:21:03 +0530

    add event listeners

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var ms = require('ms'),
-	co = require('co');
+	co = require('co'),
+	Emitter = require('co-emitter');
+var emitter = new Emitter();
+
 
 function wait(ms){
-	return function(done){
-		setTimeout(done,ms);
-	}
-}
+ 	return function(done){
+ 		setTimeout(done,ms);
+ 	}
+ }
 
 module.exports = function(ms,fn){
 	if ('string' == typeof ms) ms = parse(ms);
 	co(function* (){
-		while(1){
+		emitter.on('loop',function *(){
 			yield wait(ms);
 			fn.apply();
+		});
+		co(function* (){
+		while(1){
+			yield emitter.emit('loop')
+			clearTimeout();
 		}
+		})();
 	})();
+
 };
